04528af29c39a3d55ba9e9511ac48554db5a6666
Micael-William <mw931991@gmail.com>
2023-10-04 11:55:23 -0300

    feat: funcionalidade de ir para o próximo anúncio e desabilitaro anúncio anterior para não ser exibido

---
+++ marcas.js
@@ -0,0 +1,26 @@
+let config = [
+    bk = {
+        name: 'Burguer King',
+        img: 'bk.png'
+    },
+    taxi = {
+        name: '99 Taxi',
+        img: '99.png'
+    },
+    adidas = {
+        name: 'Adidas',
+        img: 'adidas.webp'
+    },
+    amazon = {
+        name: 'Amazon',
+        img: 'amazon.webp'
+    },
+    gucci = {
+        name: 'Gucci',
+        img: 'gucci.png'
+    },
+    record = {
+        name: 'Record',
+        img: 'record.png'
+    },
+]
